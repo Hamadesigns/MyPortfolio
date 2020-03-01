804f10afff512a65e69610b374a51c976baa6070
Muhammad Hammad <hamad@Muhammads-MacBook-Pro.local>
2020-02-29 20:56:26 -0700

    Updates

---
+++ precache-manifest.6e9c85465c573c2a773fafd926d5da52.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa82f16b39bbec5b747488c662072654",
+    "url": "/MyPortfolio/static/media/aboutbackground1.aa82f16b.jpg"
+  },
+  {
+    "revision": "738a01d5277f4a11f4c6",
+    "url": "/MyPortfolio/static/css/main.aed12d20.chunk.css"
+  },
+  {
+    "revision": "41ef2c5e8e68d7a2a057",
+    "url": "/MyPortfolio/static/js/runtime~main.ccce04bf.js"
+  },
+  {
+    "revision": "66e76eb562027fd80b48c9790fadc0a6",
+    "url": "/MyPortfolio/static/media/preview5.66e76eb5.png"
+  },
+  {
+    "revision": "5f6b6db2a8e4a79558c1",
+    "url": "/MyPortfolio/static/js/2.497e7e49.chunk.js"
+  },
+  {
+    "revision": "ce6cf4365ed00fd7a37289fd8cff1fb2",
+    "url": "/MyPortfolio/static/media/backgorund3.ce6cf436.jpg"
+  },
+  {
+    "revision": "5bd72f5e9150ebaa17a2cb8ad219b29a",
+    "url": "/MyPortfolio/static/media/contactbackground.5bd72f5e.jpg"
+  },
+  {
+    "revision": "738a01d5277f4a11f4c6",
+    "url": "/MyPortfolio/static/js/main.a04802c2.chunk.js"
+  },
+  {
+    "revision": "3dac715e9e2e56cb1693b2c4b78b746e",
+    "url": "/MyPortfolio/static/media/preview1.3dac715e.png"
+  },
+  {
+    "revision": "b6ee606d9e89f95d327fe6bb0e723f34",
+    "url": "/MyPortfolio/static/media/preview2.b6ee606d.png"
+  },
+  {
+    "revision": "95371a30a87016aad1a162a429da4445",
+    "url": "/MyPortfolio/static/media/preview3.95371a30.png"
+  },
+  {
+    "revision": "31998bfa915b4eb2cb916682b80357dc",
+    "url": "/MyPortfolio/static/media/preview4.31998bfa.png"
+  },
+  {
+    "revision": "5f6b6db2a8e4a79558c1",
+    "url": "/MyPortfolio/static/css/2.b2173a0b.chunk.css"
+  },
+  {
+    "revision": "e11c42f53c540e04e1517fd23c8cae05",
+    "url": "/MyPortfolio/index.html"
+  }
+];
